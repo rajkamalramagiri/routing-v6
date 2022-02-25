6cd67c629257dfd3a4663da5c80c5549cda463c0
Surendra <v-surendra.raj@shutterfly.com>
2022-02-25 08:07:52 +0530

    protected route

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
+import Profile from "./pages/profile";
+
+const App = () => {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    let user = localStorage.getItem("user");
+    user && JSON.parse(user) ? setAuth(true) : setAuth(false);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", auth);
+  }, [auth]);
+
+  return (
+    <Routes>
+      {!auth && (
+        <>
+        <Route
+          path="/auth"
+          element={<Auth authenticate={() => setAuth(true)} />}
+        />
+        <Route
+          path="*"
+          element={<Auth authenticate={() => setAuth(true)} />}
+        />
+        </>
+      )}
+
+      {auth && (
+        <>
+          <Route
+            path="/profile"
+            element={<Profile logout={() => setAuth(false)} />}
+          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="*"
+            element={<Profile logout={() => setAuth(false)} />}
+          />
+
+        </>
+      )}
+    </Routes>
+  );
+};
+
+export default App;
